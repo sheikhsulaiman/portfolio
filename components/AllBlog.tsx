f0564386b9c37e66d0edbac5e841ce365f99648a
sheikhsulaimansony <sheikhsulaimansony@outlook.com>
2024-01-12 19:35:24 +0600

    responsive search bar

--- components/AllBlog.tsx
+++ components/AllBlog.tsx
@@ -3,10 +3,13 @@ import React ,{useEffect, useState} from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Combobox } from "@/components/combo-box";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Card from "./Card";
 import { Blog } from "@/sanity/sanity-utils";
+import { useBreakpoint } from "@/hooks/useBreakpoint";
+import { cn } from "@/lib/utils";
+
 
 const AllBlog = ({
   blogs,
@@ -19,22 +22,29 @@ const AllBlog = ({
 
   const [isMounted,setIsMounted]=useState(false);
   
-  const [searchString,setSearchString] = useState<string>('')
+  const [searchString,setSearchString] = useState<string>('');
+  const isMobile = useBreakpoint(1023)
+  const is2XL = useBreakpoint(1535)
+  const [showSearchInput, setShowSearchInput] = useState<boolean>(false)
 
-  useEffect(()=>{
-    setIsMounted(true)
-  },[])
+  useEffect(() => {
+    setShowSearchInput(!isMobile)
+  }, [isMobile])
 
   
-
+  
   const searchParams = useSearchParams();
-  const selectedCategory = categories.includes(
+  const selectedCategory= categories.includes(
     searchParams.get("category") as string
-  )
+    )
     ? searchParams.get("category")
     : categories[0];
     
-  if(!isMounted){
+    useEffect(()=>{
+      setIsMounted(true)
+    },[])
+
+    if(!isMounted){
       return null
     }
    
@@ -42,9 +52,9 @@ const AllBlog = ({
   return (
     <div className="w-full">
       <div className="container mt-6 mx-auto flex w-full items-center justify-between">
-        <div className="lg:hidden">
+      { !showSearchInput && <div className="lg:hidden">
           <Combobox categories={categories} />
-        </div>
+        </div>}
         <div className="hidden lg:flex items-center flex-grow gap-x-2">
           {categories.map((category, index) => (
             <Button
@@ -54,7 +64,7 @@ const AllBlog = ({
               className="capitalize "
               onClick={() => {
                 router.replace(
-                  category === "all" ? "/" : `/?category=${category}`,
+                  category === "all" ? "/blog" : `/blog?category=${category}`,
                   { scroll: false }
                 );
               }}
@@ -63,17 +73,36 @@ const AllBlog = ({
             </Button>
           ))}
         </div>
-        <form>
+        {showSearchInput && <form className="flex-grow lg:flex-grow-0">
           <div className="relative flex items-center -space-x-6">
-            <Search className="w-4 h-4 text-muted-foreground" />
+          { !isMobile && <Search className="w-4 h-4 text-muted-foreground" />}
 
             <Input
-              onChange={(e)=>{setSearchString(e.target.value)}}
-              className="peer/input block box-border w-full rounded-md shadow-sm transition-all text-foreground focus-visible:shadow-md outline-none focus:ring-current focus:ring-2 focus-visible:border-foreground-muted focus-visible:ring-background-control placeholder-foreground-muted bg-foreground/[.026] border border-control text-sm leading-4 px-3 py-2 pl-10"
-              placeholder="Search"
+              onChange={(e)=>{
+                setSearchString(e.target.value)
+              }}
+              className={cn(
+                "peer/input block box-border w-full rounded-md shadow-sm transition-all text-foreground focus-visible:shadow-md outline-none focus:ring-current focus:ring-2 focus-visible:border-foreground-muted focus-visible:ring-background-control placeholder-foreground-muted bg-foreground/[.026] border border-control text-sm leading-4 px-3 py-2 ",
+              isMobile ? 'pl-2':'pl-10'
+              )}placeholder="Search"
             />
+            {isMobile && <button  type="button" onClick={() => {
+                      setSearchString('')
+                      setShowSearchInput(false)
+                    }}>
+              <X className="w-4 h-4 text-muted-foreground hover:text-red-400 hover:cursor-pointer"/>
+            </button>}
           </div>
-        </form>
+        </form>}
+        {!showSearchInput && (
+          <Button
+            variant={"ghost"}
+            size={'icon'}
+            onClick={() => setShowSearchInput(true)}
+          >
+            <Search  className="w-4 h-4 text-muted-foreground" />
+          </Button>
+        )}
       </div>
       {blogs.length === 0 && (
         <div className="container mx-auto w-full h-40 flex items-center justify-center border rounded-md my-6 bg-slate-100 dark:bg-slate-900">
