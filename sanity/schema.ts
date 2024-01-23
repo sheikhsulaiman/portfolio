95179596b9c38597bfefc483e80617d5bf390308
sheikhsulaimansony <sheikhsulaimansony@outlook.com>
2024-01-23 16:46:28 +0600

    add sanity schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import author from './schemas/author'
+import category from './schemas/category'
+import post from './schemas/post'
+import tags from './schemas/tags'
+import blockContent from './schemas/blockContent'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author,category,post,tags,blockContent],
 }
